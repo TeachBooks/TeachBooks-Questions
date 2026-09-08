bfc875f6ffd77a8cc73e5dc3e36f8a29ff508dc6
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2026-09-08 13:41:59 +0200

    Update ComputeEngine dependency version

--- src/teachbooks_questions/_static/teachbooks_sag.js
+++ src/teachbooks_questions/_static/teachbooks_sag.js
@@ -1,6 +1,6 @@
 // Functionality for short-answer gaps questions in Teachbooks
 
-import { ComputeEngine } from "https://esm.run/@cortex-js/compute-engine@0.55.6";
+import { ComputeEngine } from "https://esm.run/@cortex-js/compute-engine@0.126.1";
 import {
   checkMathSymbolicWithStructure,
   checkAbsolutePrecision,
